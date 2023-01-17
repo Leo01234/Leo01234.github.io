ec76831aef8c1703101f19073f9bf0b96ee7164c
Leo01234 <ijkhly@163.com>
2023-01-17 15:55:38 +0800

    重写指令说明

--- rebuild.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Path .\github-pages\ -Exclude .git | Remove-Item -Recurse
-hugo --config config.github.toml
-Get-ChildItem -Path .\gitee-pages\ -Exclude .git | Remove-Item -Recurse
-hugo --config config.gitee.toml
---
+++ update_site.md
@@ -0,0 +1,3 @@
+1. 删除docs文件夹
+2. hugo --config config.github.toml
+3. git push
